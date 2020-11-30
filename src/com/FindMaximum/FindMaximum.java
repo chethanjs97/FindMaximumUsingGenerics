27995f9e1c8529b0b790708b8d112a3c762b0f68
chethanjs97 <chethanjschethu@gmail.com>
2020-11-30 09:15:53 +0530

    Added : Finding Maximum In Integer Values and Test Cases

--- src/com/FindMaximum/FindMaximum.java
+++ src/com/FindMaximum/FindMaximum.java
@@ -1,9 +1,21 @@
 package com.FindMaximum;
 
+import java.util.Scanner;
+
 public class FindMaximum {
+	
+	public static Integer findMax(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Find Maximum Program");
+		Scanner sc = new Scanner(System.in);
+		System.out.println(findMax(10, 16, 8));
 
 	}
 
