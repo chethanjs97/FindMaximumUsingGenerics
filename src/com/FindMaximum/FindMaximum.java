4ddf993d5b574d9390932bc8ac4094054d605b8b
chethanjs97 <chethanjschethu@gmail.com>
2020-11-30 09:48:07 +0530

    Added : Finding Maximum In Given Float Values and Test Cases

--- src/com/FindMaximum/FindMaximum.java
+++ src/com/FindMaximum/FindMaximum.java
@@ -3,9 +3,18 @@ package com.FindMaximum;
 import java.util.Scanner;
 
 public class FindMaximum {
-	
+
 	public static Integer findMax(Integer a, Integer b, Integer c) {
 		Integer max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
+
+	public static Float findMax(Float a, Float b, Float c) {
+		Float max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
@@ -16,6 +25,7 @@ public class FindMaximum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println(findMax(10, 16, 8));
+		System.out.println(findMax(10.1f, 16.2f, 8.0f));
 
 	}
 
