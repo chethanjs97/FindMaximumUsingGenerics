c8969dc48bb65b6f79774ff6f16ce69dab02abef
chethanjs97 <chethanjschethu@gmail.com>
2020-11-30 23:21:48 +0530

    Refactored : All the 3 to One Generic Method to Find Maximum

--- src/com/FindMaximum/FindMaximum.java
+++ src/com/FindMaximum/FindMaximum.java
@@ -4,26 +4,8 @@ import java.util.Scanner;
 
 public class FindMaximum {
 
-	public static Integer findMax(Integer a, Integer b, Integer c) {
-		Integer max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	public static Float findMax(Float a, Float b, Float c) {
-		Float max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	public static String findMax(String a, String b, String c) {
-		String max = a;
+	public static <E extends Comparable<E>> E findMax(E a, E b, E c) {
+		E max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
@@ -33,7 +15,7 @@ public class FindMaximum {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println(findMax(10, 16, 8));
+		System.out.println(findMax(10, 18, 8));
 		System.out.println(findMax(10.1f, 16.2f, 8.0f));
 		System.out.println(findMax("Apple", "Peach", "Banana"));
 
